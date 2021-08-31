c4c6527ae9fccf82af8e04ee22b5548dc2f87bf3
acte2python1edu <89832872+acte2python1edu@users.noreply.github.com>
2021-08-31 13:09:52 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -18,3 +18,33 @@ if num == sum:
    print(num,"is an Armstrong number")
 else:
    print(num,"is not an Armstrong number")
+
+   
+   
+   
+   
+   
+   #2 nd
+   
+   
+   num = 1634
+
+# Changed num variable to string, 
+# and calculated the length (number of digits)
+order = len(str(num))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
