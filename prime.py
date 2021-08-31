2b5da9e365f5754a26d9f349edde08a25cc953bb
acte2python1edu <89832872+acte2python1edu@users.noreply.github.com>
2021-08-31 13:04:51 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,97 @@
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+#if flag:
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a p)
+          
+# 2 nd
+          
+          
+# Program to check if a number is prime or not
+
+num = 407
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+          
+          
+          
+          '''Output
+
+407 is not a prime number
+11 times 37 is 407'''
+          
+          
+          
+ # Python program to display all the prime numbers within an interval
+
+lower = 900
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+          
+          
+ '''         
+ Prime numbers between 900 and 1000 are:
+907
+911
+919
+929
+937
+941
+947
+953
+967
+971
+977
+983
+991
+997
+  '''
